87d9660efa5dd64e33439c916226862223e18904
Sahil Mondol <sahilmondol95@gmail.com>
2019-08-25 23:23:15 +0530

    miscelleneous changes

--- actions/slack_api.py
+++ actions/slack_api.py
@@ -7,7 +7,7 @@ from st2common.runners.base_action import Action
 
 class slack_api(Action):
 	def run(self,message):
-		token_dict = {'access_token':'xoxp-569934227830-706838758277-730451567328-01fa5fce7c284d2116d82d46a3fd8787'}
+		token_dict = {'access_token':'xoxp-569934227830-706838758277-726197777811-cf267733aab2c5f062f0f1e176bce829'}
 		auth_token = token_dict['access_token']
 		url = "https://slack.com/api/chat.postMessage"
 		payload = {"channel": "CGQB47LKV",
